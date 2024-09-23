bc04c5164e1c2c6e1990ed4b7dab5ede4ee435b6
Nea26 <45176584+Nea26@users.noreply.github.com>
2024-09-22 20:28:54 -0600

    modificaciones al bot para manejar errores

--- bot.mjs
+++ bot.mjs
@@ -12,9 +12,19 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-bot.on('message', ({ chat: { id }, text }) => {
-  const fn = buscarAccion(text)
-  bot.sendMessage(id, fn(text));
+bot.on('message', async ({ chat: { id }, text }) => {
+  const fn = buscarAccion(text);
+  try {
+      const response = await fn(text); // Llama a la función de acción
+      if (response.endsWith('.png')) {
+          // Si la respuesta es una ruta de imagen, envíala
+          bot.sendPhoto(id, response, { caption: 'Aquí está tu código de barras para la factura que debes:' });
+      } else {
+          bot.sendMessage(id, response); // De lo contrario, envía el mensaje
+      }
+  } catch (error) {
+      bot.sendMessage(id, error.message);
+  }
 });
 
 bot.on("error", console.log)
